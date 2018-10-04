e586003f8f520cc54fe21555b64303940df9384e
Amin Shah Gilani <amin@gilani.me>
2018-10-05 01:28:47 +0500

    Ensure burger menu works

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,48 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-const Header = ({ noHeader, siteTitle }) => {
-  if (noHeader) {
-    return ''
-  } else {
-    return (
-      <nav class="has-background-light navbar" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
-        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      burgerExpanded: false,
+    };
+  }
 
-      <div id="navbarBasicExample" class="navbar-menu">
-        <div class="navbar-end">
-          <Link to="/" className="navbar-item">
-            Home
-          </Link>
+  toggleBurger() {
+    this.setState({
+      burgerExpanded: !this.state.burgerExpanded,
+    })
+  }
 
-          <a class="navbar-item" target="_blank" rel="noopener noreferrer" href="https://www.toptal.com/resume/amin-shah-gilani#contract-just-respected-software-architects">
-            Resume
-          </a>
+  render() {
+
+    const activeClass = this.state.burgerExpanded ? 'is-active' : '';
+      return (
+        <nav className="has-background-light navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <div role="button" onClick={this.toggleBurger.bind(this)} className={`navbar-burger burger ${activeClass}`} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </div>
         </div>
-      </div>
-    </nav>
-    )
+
+        <div id="navbarBasicExample" className={`navbar-menu ${activeClass}`}>
+          <div className="navbar-end">
+            <Link to="/" className="navbar-item">
+              Home
+            </Link>
+
+            <a className="navbar-item" target="_blank" rel="noopener noreferrer" href="https://www.toptal.com/resume/amin-shah-gilani#contract-just-respected-software-architects">
+              Resume
+            </a>
+          </div>
+        </div>
+      </nav>
+    );
   }
 }
+
 
 export default Header
