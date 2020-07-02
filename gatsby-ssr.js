111e67fc32f2d3506d2402653b8e709ce75fe44b
Amin Shah Gilani <amin@gilani.me>
2020-07-02 23:04:45 +0500

    Move mailchimp script to SSR

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,20 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+import React from "react"
+export const onRenderBody = (
+ { setHeadComponents, setPostBodyComponents },
+ pluginOptions
+) => {
+ setHeadComponents([
+   <script
+      key="mailchimp"
+      id="mcjs"
+      dangerouslySetInnerHTML={{
+        __html: `
+        !function(c,h,i,m,p){var m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/9f8b3faec654f3522dfef4059/245eec5628f6ecc631381696f.js");
+      `,
+      }}
+   />,
+ ])
+}
