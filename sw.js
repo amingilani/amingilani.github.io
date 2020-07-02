6f37dcc189f258b849400b384d7110f48490f715
Amin Shah Gilani <amin@gilani.me>
2020-07-02 09:02:09 +0500

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-fa84f3b3f9b787b1e50d.js"
+    "url": "webpack-runtime-f01b1539971a6824b7e3.js"
   },
   {
     "url": "framework-8dcecaaefd71e2213eb2.js"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "fb1177afb823e527dba7a4dead738ebf"
+    "revision": "93f45090c0b100bfb158478f9a1a4c91"
   },
   {
     "url": "manifest.webmanifest",
